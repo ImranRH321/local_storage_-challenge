79ae26a1177d5b87842821ddd303329e5d166fc9
ImranRH321 <imranrh247@gmail.com>
2023-04-07 21:23:21 +0600

    fist-chalng done

--- local.js
+++ local.js
@@ -5,7 +5,24 @@ localStorage.setItem("name", JSON.stringify("Shakib khan"));
 // challenge: 2;
 localStorage.setItem("age", JSON.stringify(40))
 // challenge: 3;
-const name = localStorage.getItem("name")
-const age = localStorage.getItem("age")
-console.log(name);
-console.log(age);
+const names = localStorage.getItem("name")
+const ages = localStorage.getItem("age")
+
+const name = JSON.parse(names)
+const age = JSON.parse(ages)
+
+// console.log(name);
+// console.log(age);
+// ----------
+// challenge: 4 -item remove  key and value;
+// localStorage.removeItem('name')
+// localStorage.removeItem('age')
+// ------------- 
+// challenge: 5 set object --> 
+const person = {firstName: "Abraham", lastName: "linkon"}
+
+// console.log(persons); 
+localStorage.setItem("person", JSON.stringify(person))
+// done new lean first change //
+
+
