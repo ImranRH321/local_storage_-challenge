bf9dae47a7a713eb21fae2ec4288593816ccb84e
ImranRH321 <imranrh247@gmail.com>
2023-04-07 23:06:09 +0600

    chalange-2-is face-its-end

--- local.js
+++ local.js
@@ -3,13 +3,13 @@
 // challenge: 1;
 localStorage.setItem("name", JSON.stringify("Shakib khan"));
 // challenge: 2;
-localStorage.setItem("age", JSON.stringify(40))
+localStorage.setItem("age", JSON.stringify(40));
 // challenge: 3;
-const names = localStorage.getItem("name")
-const ages = localStorage.getItem("age")
+const names = localStorage.getItem("name");
+const ages = localStorage.getItem("age");
 
-const name = JSON.parse(names)
-const age = JSON.parse(ages)
+const name = JSON.parse(names);
+const age = JSON.parse(ages);
 
 // console.log(name);
 // console.log(age);
@@ -17,12 +17,29 @@ const age = JSON.parse(ages)
 // challenge: 4 -item remove  key and value;
 // localStorage.removeItem('name')
 // localStorage.removeItem('age')
-// ------------- 
-// challenge: 5 set object --> 
-const person = {firstName: "Abraham", lastName: "linkon"}
+// -------------
+// challenge: 5 set object -->
+const person = { firstName: "Abraham", lastName: "linkon" };
 
-// console.log(persons); 
-localStorage.setItem("person", JSON.stringify(person))
+// console.log(persons);
+localStorage.setItem("person", JSON.stringify(person));
 // done new lean first change //
 
+// ======================
+// level 2 challenge //
+// ========================
+const incrementValue = () => {
+  const p = document.getElementById("value");
+  const textValue = parseInt(p.innerText) + 1;
+  localStorage.setItem("id", JSON.stringify(textValue));
+  console.log(textValue);
+  p.innerText = textValue;
+};
 
+const displayDataValueReload = () => {
+  const p = document.getElementById("value");
+  const getD = localStorage.getItem("id");
+  //    console.log(p, typeof getD);
+  p.innerText = JSON.parse(getD);
+};
+displayDataValueReload();
